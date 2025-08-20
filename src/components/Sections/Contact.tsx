4ef1c699d6862f8796d47f74cee6856a6951a6a8
Shashikiran <devadigashashikiran@gmail.com>
2025-08-20 18:45:08 -0400

    Update location in Contact component

---
+++ src/components/Sections/Contact.tsx
@@ -0,0 +1,5 @@
+@@ .. @@
+               <MapPin className="w-5 h-5 text-blue-400" />
+-              <span>Philadelphia, Pennsylvania</span>
++              <span>Seattle, Washington</span>
+             </div>
